e345844d00b96066dc966739ef343399cc92eb28
Pia Freilinger <pia.freilinger@gmx.de>
2021-12-16 00:56:00 +0100

    fix jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -2,4 +2,10 @@ node {
     stage('Checkout scm') {
         checkout scm
     }
+    stage('Build') {
+        sh "mvn clean install"
+    }
+    stage('Start docker') {
+        sh "docker-compose build && docker-compose up"
+    }
 }
