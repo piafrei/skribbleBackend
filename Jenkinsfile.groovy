b026adf846b8a35550822b60207d53e5d1991229
Pia Freilinger <pia.freilinger@gmx.de>
2021-12-16 02:04:52 +0100

    start in background

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -6,6 +6,6 @@ node {
         sh "mvn clean install"
     }
     stage('Start docker') {
-        sh "docker-compose build && docker-compose up"
+        sh "docker-compose build && docker-compose up -d"
     }
 }
