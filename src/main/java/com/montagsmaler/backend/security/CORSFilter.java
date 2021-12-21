857478036625bf74439d8fe2b5a3b1ace35cae8f
Pia Freilinger <pia.freilinger@gmx.de>
2021-12-21 21:21:39 +0100

    test with sec

--- src/main/java/com/montagsmaler/backend/security/CORSFilter.java
+++ src/main/java/com/montagsmaler/backend/security/CORSFilter.java
@@ -25,7 +25,7 @@ public class CORSFilter implements Filter {
 
             // Access-Control-Allow-Origin
             String origin = request.getHeader("Origin");
-            response.setHeader("Access-Control-Allow-Origin", "https://p-frei.de");
+            response.setHeader("Access-Control-Allow-Origin", "http://p-frei.de");
             response.setHeader("Vary", "Origin");
 
             // Access-Control-Max-Age
