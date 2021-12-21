9af8aa388bb7f7cf14886bdfabaf8e24f1b22a0a
Pia Freilinger <pia.freilinger@gmx.de>
2021-12-21 20:35:57 +0100

    test with sec

--- src/main/java/com/montagsmaler/backend/security/SecurityConfiguration.java
+++ src/main/java/com/montagsmaler/backend/security/SecurityConfiguration.java
@@ -33,7 +33,13 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
+        http.cors().configurationSource(request -> {
+                var cors = new CorsConfiguration();
+                cors.setAllowedOrigins(Arrays.asList("*"));
+                cors.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "OPTIONS"));
+                cors.setAllowedHeaders(Arrays.asList("*"));
+                return cors;
+                }).and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/backend/user/signup").permitAll()
                 .antMatchers("/backend/user/authenticate").permitAll()
