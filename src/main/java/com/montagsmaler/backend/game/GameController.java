389fe9e4f8fce449a512f1d51ed23484ac8c0a70
Pia Freilinger <pia.freilinger@gmx.de>
2022-01-04 22:46:33 +0100

    hartcode user (until auth is fixed)

--- src/main/java/com/montagsmaler/backend/game/GameController.java
+++ src/main/java/com/montagsmaler/backend/game/GameController.java
@@ -99,7 +99,8 @@ public class GameController {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         System.out.println(authentication.getName());
 
-        action.setUsername(authentication.getName());
+        action.setUsername("Maia");
+        //action.setUsername(authentication.getName());
 
         return strategy.executeAction(action);
     }
