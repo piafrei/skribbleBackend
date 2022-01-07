3a67506266f83351198f26123addf66a14438b32
Pia Freilinger <pia.freilinger@gmx.de>
2022-01-07 19:20:09 +0100

    permit all without token

--- src/main/java/com/montagsmaler/backend/security/WebSocketSecurityConf.java
+++ src/main/java/com/montagsmaler/backend/security/WebSocketSecurityConf.java
@@ -1,7 +1,6 @@
 package com.montagsmaler.backend.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 
@@ -9,7 +8,7 @@ import org.springframework.security.config.annotation.web.socket.AbstractSecurit
 public class WebSocketSecurityConf extends AbstractSecurityWebSocketMessageBrokerConfigurer {
     @Override
     protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
-        //messages.anyMessage().permitAll();
+        messages.anyMessage().permitAll();
     }
 
     @Override
